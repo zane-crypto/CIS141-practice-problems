414e4f342f2b8dcc447e6f69632f93e8ef3f67ac
zane-crypto <zaneadlum@student.olympic.edu>
2025-04-18 13:24:03 -0700

    Update Problem1.py

--- Mod2/PracticeProb/Problem1.py
+++ Mod2/PracticeProb/Problem1.py
@@ -1,1 +1,8 @@
-
+'''
+1. Create a program that prints the following output to the screen:
+"Water. Earth. Fire. Air. Long ago, the four nations lived together
+in harmony. Then, everything changed when the Fire Nation attacked."
+'''
+print("""Water. Earth. Fire. Air. Long ago, the four nations lived 
+together in harmony. Then, everything changed when the Fire Nation 
+attacked.""")
