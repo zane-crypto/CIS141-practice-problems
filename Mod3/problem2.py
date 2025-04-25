37dc3652603c4a9ee9f34d974ab1f125b8e98fa1
zane-crypto <zaneadlum@student.olympic.edu>
2025-04-24 22:53:40 -0700

    Update problem2.py

--- Mod3/problem2.py
+++ Mod3/problem2.py
@@ -3,3 +3,10 @@
 Use string concatenation to print "Hello, <name>! You are <age1> years old. 
 Next year, you will be <age2> years old."
 '''
+
+name = input("What is your name? ")
+age1 = input("How old are you? ")
+age1int = int(age1)
+age2int = age1int + 1
+age2str = str(age2int)
+print("Hello, " + name + "! You are " + age1 + " years old. Next year, you will be " + age2str + " years old.")
