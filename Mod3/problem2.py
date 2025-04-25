00b9640a607dea3d3865b4caf6295d4b7a7a59c2
zane-crypto <zaneadlum@student.olympic.edu>
2025-04-24 20:19:56 -0700

    Create problem2.py

---
+++ Mod3/problem2.py
@@ -0,0 +1,5 @@
+'''
+2. Prompt the user for their name and their age. Calculate their age next year. 
+Use string concatenation to print "Hello, <name>! You are <age1> years old. 
+Next year, you will be <age2> years old."
+'''
