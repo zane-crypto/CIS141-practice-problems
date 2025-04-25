326db662e04116d4f62c6abfa3ad3ba4720d011c
zane-crypto <zaneadlum@student.olympic.edu>
2025-04-24 20:19:13 -0700

    Create Problem1.py

---
+++ Mod3/Problem1.py
@@ -0,0 +1,5 @@
+'''
+1. Prompt the user for a word. Then, prompt the user for how many times 
+they'd like that word repeated. Use the skills you learned in this module
+to print the word the correct number of times.
+'''
