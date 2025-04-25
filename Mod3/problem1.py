ae2bfc371cf286b5fa584df1576fb877a2adc9e6
zane-crypto <zaneadlum@student.olympic.edu>
2025-04-24 22:13:27 -0700

    Update problem1.py

--- Mod3/problem1.py
+++ Mod3/problem1.py
@@ -3,3 +3,8 @@
 they'd like that word repeated. Use the skills you learned in this module
 to print the word the correct number of times.
 '''
+
+str1 = input("pick a word, any word: ")
+num1 = int(input("how many times would you you like the word repeated? "))
+
+print(str1 * num1)
