0df2ac02236aca4179bf7a6f50984cd114b4ea3c
zane-crypto <zaneadlum@student.olympic.edu>
2025-04-26 17:32:03 -0700

    Update problem5.py

--- Mod3/problem5.py
+++ Mod3/problem5.py
@@ -1,5 +1,11 @@
 '''
-5. Print 3 words with a | character (called a pipe) in between them. 
+5. Print 3 words with a | character (called a pipe) in between them.
 Use the appropriate keyword argument in print() to do so.
 '''
 
+wrd1 = ("American Clearing House")
+new_wrd1 = wrd1.split(" ")
+print(new_wrd1)
+
+new_wrd2 = "|".join(new_wrd1)
+print(new_wrd2)
