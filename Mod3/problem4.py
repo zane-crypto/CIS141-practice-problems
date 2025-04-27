8168f4a9fc5632f2dffd358fe210f08f7713f956
zane-crypto <zaneadlum@student.olympic.edu>
2025-04-26 17:31:23 -0700

    Update problem4.py

--- Mod3/problem4.py
+++ Mod3/problem4.py
@@ -2,3 +2,9 @@
 4. Prompt the user for: a word, a first index, and a last index.
 Slice the word at the indexes provided by the user and print to the screen.
 '''
+
+wrd1 = input("Please enter a word: ")
+indx1 = int(input("Please enter a starting number(index): "))
+indx2 = int(input("Please enter a ending number(index): "))
+
+print(wrd1[indx1:indx2])
