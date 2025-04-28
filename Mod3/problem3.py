a2a01693c96457398f7b02e89b9fef4c74ea890b
zane-crypto <zaneadlum@student.olympic.edu>
2025-04-27 19:25:45 -0700

    Update problem3.py

--- Mod3/problem3.py
+++ Mod3/problem3.py
@@ -1,11 +1,11 @@
 '''
-3. Prompt the user for a sentence and a word to try 
+3. Prompt the user for a sentence and a word to try
 to find in that sentence. Have the program print out
-whether the word was found in the sentence. 
+whether the word was found in the sentence.
 (i.e. True or False)
 '''
 
 str1 = input("Type in a sentence: ")
 wrd1 = input("Type in a word to search for: ")
 bool = str(wrd1 in str1)
-print("Is the word " + wrd1 + " found in the given sentence? " + bool)
+print("Is the word '" + wrd1 + "' found in the given sentence? " + bool)
