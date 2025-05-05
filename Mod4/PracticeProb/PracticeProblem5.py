15af360372b1e89bfa170e92fd18d9f225eff433
zane-crypto <zaneadlum@student.olympic.edu>
2025-05-04 19:55:14 -0700

    Update PracticeProblem5.py

--- Mod4/PracticeProb/PracticeProblem5.py
+++ Mod4/PracticeProb/PracticeProblem5.py
@@ -1,1 +1,15 @@
+'''
+5. A store charges $5 for shipping on any order under $50.
+If the order amount is $50 or more, shipping is free.
+Ask the user for the order total and print the total cost,
+including shipping.
+'''
 
+cost = int(input("Please enter the order total: "))
+
+if (cost < 50):
+    cost = (cost+5)
+    print("Your cost including shipping ", cost)
+else:
+    print("You get free shipping! Total cost is : ", cost)
+  
