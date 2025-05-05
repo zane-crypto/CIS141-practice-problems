5655d9ce462d38bb66237ec9a1d82054f75ba487
zane-crypto <zaneadlum@student.olympic.edu>
2025-05-04 19:54:14 -0700

    Update PracticeProblem3.py

--- Mod4/PracticeProb/PracticeProblem3.py
+++ Mod4/PracticeProb/PracticeProblem3.py
@@ -1,1 +1,13 @@
+'''
+Prompt the user for their bank balance.
+Evaluate whether the balance is less
+than $100. Print True if the user’s
+balance is below $100; print False,
+otherwise
+'''
 
+balance = int(input("Please enter your bank balance: "))
+if balance < 100:
+    print("true")
+else:
+    print("false")
