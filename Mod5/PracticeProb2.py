79e0c9784ef9cf474f99158042a8e01ca24bdf17
zane-crypto <zaneadlum@student.olympic.edu>
2025-05-09 01:56:05 -0700

    Create PracticeProb2.py

---
+++ Mod5/PracticeProb2.py
@@ -0,0 +1,11 @@
+'''
+2. Define a string variable (e.g., my_string = "Olympic College").
+Use a for loop to print each character on its own line. Convert
+each character to uppercase before printing.
+'''
+
+string = "olympic college"
+
+for char in string:
+    print(char.upper())
+else: print("Ta da!")
