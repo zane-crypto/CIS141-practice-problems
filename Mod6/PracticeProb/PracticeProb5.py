122388d59cef9d382916de93dcff33aa01344049
zane-crypto <zaneadlum@student.olympic.edu>
2025-05-17 06:46:00 -0700

    Update PracticeProb5.py

--- Mod6/PracticeProb/PracticeProb5.py
+++ Mod6/PracticeProb/PracticeProb5.py
@@ -1,1 +1,11 @@
+'''
+5. Create a list of integers. Use a loop to build a new list
+where each element is the square of the corresponding element
+in the original list. Print the new list.
+'''
 
+int = [3,7,1,9,4,6,5,8,2,0]
+square = []
+for x in int:
+    square.append(x*x)
+print(square)
