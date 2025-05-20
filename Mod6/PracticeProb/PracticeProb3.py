72c58ed19b195ffc4090f8350014440c544ce330
zane-crypto <zaneadlum@student.olympic.edu>
2025-05-20 12:31:45 -0700

    Update PracticeProb3.py

--- Mod6/PracticeProb/PracticeProb3.py
+++ Mod6/PracticeProb/PracticeProb3.py
@@ -7,7 +7,7 @@ Print the resulting filtered list.
 words = ["cat", "bird", "dog", "snake"]
 word3 = []
 for word in words:
-    if len(word) <= 3:
+    if len(word) > 3:
         word3.append(word)
 
 for x in word3:
