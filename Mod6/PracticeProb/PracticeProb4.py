49f4236f2f15a15fa6f5450bad51b6a32865d505
zane-crypto <zaneadlum@student.olympic.edu>
2025-05-20 12:32:36 -0700

    Update PracticeProb4.py

--- Mod6/PracticeProb/PracticeProb4.py
+++ Mod6/PracticeProb/PracticeProb4.py
@@ -3,13 +3,13 @@
 numbers are positive and how many are negative, then print both counts.
 '''
 
-int = [3,7,1,9,4,6,5,8,2,0]
-even = 0
-odd = 0
+int = [3,7,-1,9,-4,-6,5,8,-2,0]
+pos = 0
+neg = 0
 for num in int:
-    if num % 2 == 0:
-        even += 1
+    if num >= 0:
+        pos += 1
     else:
-        odd += 1
-print(f"there are {even} even numbers.")
-print(f"there are {odd} odd numbers.")
+        neg += 1
+print(f"there are {pos} positive numbers.")
+print(f"there are {neg} negative numbers.")
